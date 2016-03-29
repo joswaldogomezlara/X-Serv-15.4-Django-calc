0f0796359109a1ddf0f3ca824c4b7c90ce78c606
Joswaldo <joswaldogomezlara@gmail.com>
2016-03-29 13:50:59 +0200

    Ejercicio 15.4

--- project/urls.py
+++ project/urls.py
@@ -6,5 +6,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^(?P<num1>[0-9]+)\+(?P<num2>[0-9]+)/$', 'calc.views.sumar'),
+    url(r'^(?P<num1>[0-9]+)\-(?P<num2>[0-9]+)/$', 'calc.views.restar'),
+    url(r'^(?P<num1>[0-9]+)\*(?P<num2>[0-9]+)/$', 'calc.views.multiplicar'),
+    url(r'^(?P<num1>[0-9]+)\/(?P<num2>[0-9]+)/$', 'calc.views.dividir'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'.*', 'calc.views.NotFound'),
 )
