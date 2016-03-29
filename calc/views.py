20138fa990948b9d80107945ab4cee82209740e3
Joswaldo <joswaldogomezlara@gmail.com>
2016-03-29 13:50:43 +0200

    Ejercicio 15.4

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def sumar(request, num1, num2):
+    return HttpResponse(num1 + ' + ' + num2 + ' = ' + str(int(num1) + int(num2)))
+
+def restar(request, num1, num2):
+    return HttpResponse(num1 + ' - ' + num2 + ' = ' + str(int(num1) - int(num2)))
+
+def multiplicar(request, num1, num2):
+    return HttpResponse(num1 + ' * ' + num2 + ' = ' + str(int(num1) * int(num2)))
+
+def dividir(request, num1, num2):
+    try:
+        return HttpResponse(num1 + ' / ' + num2 + ' = ' + str(int(num1) / int(num2)))
+    except:
+        return HttpResponse('Estas intentando dividir entre 0')
+
+def NotFound(request):
+    return HttpResponse('<h3>No existe la operacion que solicitas</h3>')
